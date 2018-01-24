da111efdfa30ff004daeae338c7123e17133e98b
Christopher Johnson <chrisajohnson1988@gmail.com>
2018-01-24 21:45:43 +0000

    Sending notification over multicast

--- doorbell_listen.py
+++ doorbell_listen.py
@@ -3,13 +3,18 @@ import time
 import os
 import requests
 import serial
+import socket
 from datetime import datetime
 
 PULSE_WINDOW = 3
 TRIGGER_COUNT = 3
+MCAST_GRP = '224.0.0.1'
+MCAST_PORT = 5007
 
 ser = serial.Serial('/dev/ttyAMA0', 9600)
 devid = os.environ['PUSHING_BOX_KEY']
+multicast = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+multicast.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
 
 def pulse():
   now = time.time()
@@ -26,6 +31,7 @@ pulse.count = 0
 def ring():
   now = str(datetime.now())
   print "[INFO] Ring Ring @ " + now
+  multicast.sendto("doorbell", (MCAST_GRP, MCAST_PORT))
   requests.get('http://api.pushingbox.com/pushingbox?devid=' + devid + '&time=' + now)
 
 while True:
